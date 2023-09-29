1d7d924a061b7bb2d6650c0784dace9a64c2aff0
Victor <vtrownz@hotmail.com>
2023-09-28 21:27:00 -0300

    REACT

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,6 @@
 import HomePage from './HomePage.tsx';
 import LoginPage from './LoginPage.tsx';
 import NotFoundPage from './NotFoundPage.tsx';
+import OAuth0LoginPage from './OAuth0LoginPage.tsx';
 
-export {
-  HomePage,
-  LoginPage,
-  NotFoundPage,
-}
+export { HomePage, LoginPage, NotFoundPage, OAuth0LoginPage };
