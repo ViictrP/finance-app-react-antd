51440f274cf6dbb73793b4ed3b62192f46d51183
Victor <victor-prado@arc-one.com>
2023-10-09 17:42:34 -0300

    Displaying an error message if getProfile fails

--- src/stores/thunks/getUserProfileThunk.ts
+++ src/stores/thunks/getUserProfileThunk.ts
@@ -1,6 +1,7 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {api} from '../../lib';
 import {UserDTO} from '../../dto';
+import {notification} from "antd";
 
 const getUserProfileThunk = createAsyncThunk(
   'get/userProfile',
@@ -13,6 +14,10 @@ const getUserProfileThunk = createAsyncThunk(
         '[getUserProfile] an error ocurred while trying to get logged user profile',
         error
       );
+      notification.error({
+        message: 'Erro ao buscar perfil',
+        description: 'Não foi possível obter o perfil do usuário. Tente novamente mais tarde.',
+      });
       return thunkApi.rejectWithValue(error);
     }
   }
